d4e0c4255b756d90d91f6b52cc6102ad3e0d761b
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-09-03 20:26:06 +0200

    establishing database connection

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const mongoose = require('mongoose')
+const app=express()
+const port=3000
+
+//establishing connection to database
+const dbURI =
+  "mongodb+srv://gloriaumutoni54:fFF2RWZYkKFP1p6o@cluster0.8k6l5.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(dbURI).then(()=>{app.listen(port)}).catch((err)=>err)
