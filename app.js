b3777c6a9af5f9c390417479e515b8991483ccdd
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-09-03 20:31:20 +0200

    establishing database connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const app=express()
 const port=3000
 
+
 //establishing connection to database
 const dbURI =
   "mongodb+srv://gloriaumutoni54:fFF2RWZYkKFP1p6o@cluster0.8k6l5.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0";
